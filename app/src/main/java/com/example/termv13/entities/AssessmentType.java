4a34c70b782b3682a4d6691fc85b393e8f3c8f64
nedious <ned.imhoff@gmail.com>
2023-09-28 20:16:39 -0400

    entities updated

---
+++ app/src/main/java/com/example/termv13/entities/AssessmentType.java
@@ -0,0 +1,17 @@
+package com.example.termv13.entities;
+
+public enum AssessmentType {
+    OA {
+        @Override
+        public String toString() {
+            return "Objective Assessment";
+        }
+    },
+
+    PA {
+        @Override
+        public String toString() {
+            return "Performance Assessment";
+        }
+    }
+}
---
+++ app/src/main/java/com/example/termv13/entities/TermEntity.java
@@ -0,0 +1,57 @@
+package com.example.termv13.entities;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.time.LocalDate;
+
+@Entity(tableName = "term_table")
+
+public class TermEntity {
+    @PrimaryKey(autoGenerate = true)
+
+    private int termID;
+
+    private String termTitle;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    public TermEntity(int termID, String termTitle, LocalDate startDate, LocalDate endDate) {
+        this.termID = termID;
+        this.termTitle = termTitle;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public int getTermID() {
+        return termID;
+    }
+
+    public void setTermID(int termID) {
+        this.termID = termID;
+    }
+
+    public String getTermTitle() {
+        return termTitle;
+    }
+
+    public void setTermTitle(String termTitle) {
+        this.termTitle = termTitle;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+}
