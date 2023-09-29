7101af8642baa82486b82485d50a44ca4bbfcdb1
nedious <ned.imhoff@gmail.com>
2023-09-28 20:57:00 -0400

    entities updated

--- app/src/main/java/com/example/termv13/UI/ActivityTermList.java
+++ app/src/main/java/com/example/termv13/UI/ActivityTermList.java
@@ -1,14 +1,16 @@
-package com.example.termv13;
+package com.example.termv13.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.termv13.R;
+
+public class ActivityTermList extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_term_list);
     }
 }
